29f464fa0e431553968e583070c736b115a26c06
knandas <nandhasri.k@gmail.com>
2025-04-28 01:26:58 +0700

    Update http.c

--- http.c
+++ http.c
@@ -1,3 +1,5 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////    esp32-http-request get   /////////////////////////////////////////////////////
 /*
  esp32-http-request
 */
@@ -33,7 +35,9 @@ void setup()
   // httpCode will be negative on error
   if(httpCode > 0) {
     // file found at server
-    if(httpCode == HTTP_CODE_OK) {
+    if(httpCode == HTTP_CODE_OK) 
+    {
+      Serial.println("Payload:");
       String payload = http.getString();
       Serial.println(payload);
     } else {
@@ -53,4 +57,92 @@ void loop()
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////    esp32-http-request get json   /////////////////////////////////////////////////////
+/*
+ esp32-http-request json
+*/
+
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <Arduino_JSON.h>
+
+const char WIFI_SSID[] = "ELEC302";            
+const char WIFI_PASSWORD[] = "elec1234"; 
+
+String HOST_NAME   = "http://httpbin.org/get"; 
+String PATH_NAME   = "?temp=34.56";      
+
+void setup() 
+{
+  Serial.begin(115200); 
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+  
+  HTTPClient http;
+
+  http.begin(HOST_NAME + PATH_NAME); //HTTP
+  int httpCode = http.GET();
+
+  // httpCode will be negative on error
+  if(httpCode > 0) 
+  {
+    // file found at server
+    if(httpCode == HTTP_CODE_OK) 
+    {
+      Serial.println("Payload:");
+      String payload = http.getString();
+      Serial.println(payload);
+
+      Serial.println("parse");
+      Serial.println("=====");
+      JSONVar myObject = JSON.parse(payload);
+
+      // JSON.typeof(jsonVar) can be used to get the type of the variable
+      if (JSON.typeof(myObject) == "undefined") 
+      {
+       Serial.println("Parsing input failed!");
+       return;
+      }
+      
+      
+      //if (myObject.hasOwn("temp"))
+      {
+        Serial.print("\"temp\" = ");
+        Serial.println(myObject["args"]["temp"]);
+        String conv = myObject["args"]["temp"];
+        Serial.println(conv);
+        float C =conv.toFloat();
+        Serial.println(C);
+
+      }
+
+    } else 
+    {
+      // HTTP header has been send and Server response header has been handled
+      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+    }
+  } 
+  else 
+  {
+   Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
+
+  http.end();
+}
+
+void loop() 
+{
+
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
