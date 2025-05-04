a26c932ea5fb49bc558f0353f691247184d09a90
knandas <nandhasri.k@gmail.com>
2025-05-04 20:04:31 +0700

    Update webSocket.c

--- webSocket.c
+++ webSocket.c
@@ -748,4 +748,348 @@ void loop()
 }
 
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////// esp32_webSocket_RCbot.ino /////////////////////////////////////////////////////////////////
+
+/*
+ * This ESP32 code is created by esp32io.com
+ *
+ * This ESP32 code is released in the public domain
+ *
+ * For more detail (instruction and wiring diagram), visit https://esp32io.com/tutorials/esp32-controls-car-via-web
+ */
+
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <WebSocketsServer.h>
+#include "index.h"
+
+#define CMD_STOP 0
+#define CMD_FORWARD 1
+#define CMD_BACKWARD 2
+#define CMD_LEFT 4
+#define CMD_RIGHT 8
+
+#define ENA_PIN 14  // The ESP32 pin GPIO14 connected to the ENA pin L298N
+#define IN1_PIN 27  // The ESP32 pin GPIO27 connected to the IN1 pin L298N
+#define IN2_PIN 26  // The ESP32 pin GPIO26 connected to the IN2 pin L298N
+#define IN3_PIN 25  // The ESP32 pin GPIO25 connected to the IN3 pin L298N
+#define IN4_PIN 33  // The ESP32 pin GPIO33 connected to the IN4 pin L298N
+#define ENB_PIN 32  // The ESP32 pin GPIO32 connected to the ENB pin L298N
+
+const char* ssid = "YOUR_WIFI_SSID";     // CHANGE IT
+const char* password = "YOUR_WIFI_PASSWORD";  // CHANGE IT
+
+AsyncWebServer server(80);
+WebSocketsServer webSocket = WebSocketsServer(81);  // WebSocket server on port 81
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t* payload, size_t length) {
+  switch (type) {
+    case WStype_DISCONNECTED:
+      Serial.printf("[%u] Disconnected!\n", num);
+      break;
+    case WStype_CONNECTED:
+      {
+        IPAddress ip = webSocket.remoteIP(num);
+        Serial.printf("[%u] Connected from %d.%d.%d.%d\n", num, ip[0], ip[1], ip[2], ip[3]);
+      }
+      break;
+    case WStype_TEXT:
+      //Serial.printf("[%u] Received text: %s\n", num, payload);
+      String angle = String((char*)payload);
+      int command = angle.toInt();
+      Serial.print("command: ");
+      Serial.println(command);
+
+      switch (command) {
+        case CMD_STOP:
+          Serial.println("Stop");
+          CAR_stop();
+          break;
+        case CMD_FORWARD:
+          Serial.println("Move Forward");
+          CAR_moveForward();
+          break;
+        case CMD_BACKWARD:
+          Serial.println("Move Backward");
+          CAR_moveBackward();
+          break;
+        case CMD_LEFT:
+          Serial.println("Turn Left");
+          CAR_turnLeft();
+          break;
+        case CMD_RIGHT:
+          Serial.println("Turn Right");
+          CAR_turnRight();
+          break;
+        default:
+          Serial.println("Unknown command");
+      }
+
+      break;
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(ENA_PIN, OUTPUT);
+  pinMode(IN1_PIN, OUTPUT);
+  pinMode(IN2_PIN, OUTPUT);
+  pinMode(IN3_PIN, OUTPUT);
+  pinMode(IN4_PIN, OUTPUT);
+  pinMode(ENB_PIN, OUTPUT);
+
+  digitalWrite(ENA_PIN, HIGH);  // set full speed
+  digitalWrite(ENB_PIN, HIGH);  // set full speed
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
+  // Initialize WebSocket server
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
+
+  // Serve a basic HTML page with JavaScript to create the WebSocket connection
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest* request) {
+    Serial.println("Web Server: received a web page request");
+    String html = HTML_CONTENT;  // Use the HTML content from the servo_html.h file
+    request->send(200, "text/html", html);
+  });
+
+  server.begin();
+  Serial.print("ESP32 Web Server's IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  webSocket.loop();
+  // TO DO: Your code here
+}
+
+void CAR_moveForward() {
+  digitalWrite(IN1_PIN, HIGH);
+  digitalWrite(IN2_PIN, LOW);
+  digitalWrite(IN3_PIN, HIGH);
+  digitalWrite(IN4_PIN, LOW);
+}
+
+void CAR_moveBackward() {
+  digitalWrite(IN1_PIN, LOW);
+  digitalWrite(IN2_PIN, HIGH);
+  digitalWrite(IN3_PIN, LOW);
+  digitalWrite(IN4_PIN, HIGH);
+}
+
+void CAR_turnLeft() {
+  digitalWrite(IN1_PIN, HIGH);
+  digitalWrite(IN2_PIN, LOW);
+  digitalWrite(IN3_PIN, LOW);
+  digitalWrite(IN4_PIN, LOW);
+}
+
+void CAR_turnRight() {
+  digitalWrite(IN1_PIN, LOW);
+  digitalWrite(IN2_PIN, LOW);
+  digitalWrite(IN3_PIN, HIGH);
+  digitalWrite(IN4_PIN, LOW);
+}
+
+void CAR_stop() {
+  digitalWrite(IN1_PIN, LOW);
+  digitalWrite(IN2_PIN, LOW);
+  digitalWrite(IN3_PIN, LOW);
+  digitalWrite(IN4_PIN, LOW);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////// esp32_webSocket_RCbot   index.h ////////////////////////////////////////////////////////////
+/*
+ * This ESP32 code is created by esp32io.com
+ *
+ * This ESP32 code is released in the public domain
+ *
+ * For more detail (instruction and wiring diagram), visit https://esp32io.com/tutorials/esp32-controls-car-via-web
+ */
+
+const char *HTML_CONTENT = R"=====(
+<!DOCTYPE html>
+<html>
+<head>
+<title>ESP32 Control Car via Web</title>
+<meta name="viewport" content="width=device-width, initial-scale=0.7, maximum-scale=1, user-scalable=no">
+<style type="text/css">
+body { text-align: center; font-size: 24px;}
+button { text-align: center; font-size: 24px;}
+#container {
+    margin-right: auto;
+    margin-left: auto;
+  width: 400px; 
+    height: 400px;
+    position: relative;
+    margin-bottom: 10px;
+}
+div[class^='button'] { position: absolute; }
+.button_up, .button_down { width:214px; height:104px;}
+.button_left, .button_right { width:104px; height:214px;}
+.button_stop { width:178px; height:178px;}
+.button_up {
+    background: url('https://esp32io.com/images/tutorial/up_inactive.png') no-repeat; 
+  background-size: contain; 
+    left: 200px;
+    top: 0px;
+  transform: translateX(-50%);
+}
+
+.button_down {
+    background: url('https://esp32io.com/images/tutorial/down_inactive.png') no-repeat; 
+  background-size: contain; 
+    left:200px;
+    bottom: 0px;
+  transform: translateX(-50%);
+}
+
+.button_right {
+    background: url('https://esp32io.com/images/tutorial/right_inactive.png') no-repeat;
+  background-size: contain;   
+    right: 0px;
+    top: 200px;
+  transform: translateY(-50%);
+}
+
+.button_left {
+    background: url('https://esp32io.com/images/tutorial/left_inactive.png') no-repeat; 
+  background-size: contain;
+    left:0px;
+    top: 200px;
+  transform: translateY(-50%);
+}
+
+.button_stop {
+  background: url('https://esp32io.com/images/tutorial/stop_inactive.png') no-repeat; 
+  background-size: contain;
+    left:200px;
+    top: 200px;
+  transform: translate(-50%, -50%);
+}
+</style>
+<script>
+var CMD_STOP     = 0;
+var CMD_FORWARD  = 1;
+var CMD_BACKWARD = 2;
+var CMD_LEFT     = 4;
+var CMD_RIGHT    = 8;
+var img_name_lookup = {
+  [CMD_STOP]:     "stop",
+  [CMD_FORWARD]:  "up",
+  [CMD_BACKWARD]: "down",
+  [CMD_LEFT]:     "left",
+  [CMD_RIGHT]:    "right"
+}
+var ws = null;
+
+function init() 
+{
+  
+  var container = document.querySelector("#container");
+    container.addEventListener("touchstart", mouse_down);
+    container.addEventListener("touchend", mouse_up);
+    container.addEventListener("touchcancel", mouse_up);
+    container.addEventListener("mousedown", mouse_down);
+    container.addEventListener("mouseup", mouse_up);
+    container.addEventListener("mouseout", mouse_up);    
+}
+function ws_onmessage(e_msg)
+{
+    e_msg = e_msg || window.event; // MessageEvent
+ 
+    //alert("msg : " + e_msg.data);
+}
+function ws_onopen()
+{
+  document.getElementById("ws_state").innerHTML = "OPEN";
+  document.getElementById("wc_conn").innerHTML = "Disconnect";
+}
+function ws_onclose()
+{
+  document.getElementById("ws_state").innerHTML = "CLOSED";
+  document.getElementById("wc_conn").innerHTML = "Connect";
+  console.log("socket was closed");
+  ws.onopen = null;
+  ws.onclose = null;
+  ws.onmessage = null;
+  ws = null;
+}
+function wc_onclick()
+{
+  if(ws == null)
+  {
+    ws = new WebSocket("ws://" + window.location.host + ":81");
+    document.getElementById("ws_state").innerHTML = "CONNECTING";
+    
+    ws.onopen = ws_onopen;
+    ws.onclose = ws_onclose;
+    ws.onmessage = ws_onmessage; 
+  }
+  else
+    ws.close();
+}
+function mouse_down(event) 
+{
+  if (event.target !== event.currentTarget) 
+  {
+    var id = event.target.id;
+    send_command(id);
+    event.target.style.backgroundImage = "url('https://esp32io.com/images/tutorial/" + img_name_lookup[id] + "_active.png')";
+    }
+    event.stopPropagation();    
+    event.preventDefault();    
+}
+
+function mouse_up(event) 
+{
+  if (event.target !== event.currentTarget) 
+  {
+    var id = event.target.id;
+    send_command(CMD_STOP);
+    event.target.style.backgroundImage = "url('https://esp32io.com/images/tutorial/" + img_name_lookup[id] + "_inactive.png')";
+    }
+    event.stopPropagation();   
+    event.preventDefault();    
+}
+function send_command(cmd) 
+{   
+  if(ws != null)
+    if(ws.readyState == 1)
+      ws.send(cmd + "\r\n");   
+}
+
+window.onload = init;
+</script>
+</head>
+<body>
+<h2>ESP32 - RC Car via Web</h2>
+<div id="container">
+  <div id="0" class="button_stop"></div>
+    <div id="1" class="button_up"></div>
+    <div id="2" class="button_down"></div>
+    <div id="8" class="button_right"></div>
+    <div id="4" class="button_left"></div>
+</div>
+<p>
+WebSocket : <span id="ws_state" style="color:blue">closed</span><br>
+</p>
+<button id="wc_conn" type="button" onclick="wc_onclick();">Connect</button>
+<br>
+<br>
+<div class="sponsor">Sponsored by <a href="https://amazon.com/diyables">DIYables</a></div>
+</body>
+</html>
+)=====";
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
