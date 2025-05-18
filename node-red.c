d94ff5411092e2640056caa677f02393d544b6d5
knandas <nandhasri.k@gmail.com>
2025-05-18 16:50:13 +0700

    Create node-red.c

---
+++ node-red.c
@@ -0,0 +1,50 @@
+///////////////////////////////// test weather JSON ///////////////////////////////////////////////////
+
+{
+    "coord": {
+        "lon": 100.4608,
+        "lat": 13.7442
+    },
+    "weather": [
+        {
+            "id": 804,
+            "main": "Clouds",
+            "description": "overcast clouds",
+            "icon": "04d"
+        }
+    ],
+    "base": "stations",
+    "main": {
+        "temp": 328.11,
+        "feels_like": 314.53,
+        "temp_min": 304.12,
+        "temp_max": 308.69,
+        "pressure": 1007,
+        "humidity": 52,
+        "sea_level": 1007,
+        "grnd_level": 1006
+    },
+    "visibility": 10000,
+    "wind": {
+        "speed": 1.7,
+        "deg": 139,
+        "gust": 1.37
+    },
+    "clouds": {
+        "all": 99
+    },
+    "dt": 1716258967,
+    "sys": {
+        "type": 2,
+        "id": 2093772,
+        "country": "TH",
+        "sunrise": 1716245434,
+        "sunset": 1716291554
+    },
+    "timezone": 25200,
+    "id": 1619460,
+    "name": "Bangkok Noi",
+    "cod": 200
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
