8ddf271e8409916844a0fe3db9bf8f394f1445e4
knandas <nandhasri.k@gmail.com>
2026-05-07 18:48:24 +0700

    Update BT.c

--- BT.c
+++ BT.c
@@ -1,1 +1,467 @@
+////////////////////////////////////////  BLE server  ////////////////////////////////////////////////////////
+/*
+    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleServer.cpp
+    Ported to Arduino ESP32 by Evandro Copercini
+    updates by chegewara
+*/
 
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Starting BLE work!");
+
+  BLEDevice::init("Long name works now");
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID,
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+
+  pCharacteristic->setValue("Hello World says Neil");
+  pService->start();
+  // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();  
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+  Serial.println("Characteristic defined! Now you can read it in your phone!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(2000);
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////// BLE_scan ///////////////////////////////////////////////////////////////////
+
+/*
+   Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleScan.cpp
+   Ported to Arduino ESP32 by Evandro Copercini
+*/
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+
+int scanTime = 5; //In seconds
+BLEScan* pBLEScan;
+
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+    void onResult(BLEAdvertisedDevice advertisedDevice) {
+      Serial.printf("Advertised Device: %s \n", advertisedDevice.toString().c_str());
+    }
+};
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Scanning...");
+
+  BLEDevice::init("");
+  pBLEScan = BLEDevice::getScan(); //create new scan
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
+  pBLEScan->setInterval(100);
+  pBLEScan->setWindow(99);  // less or equal setInterval value
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
+  Serial.print("Devices found: ");
+  Serial.println(foundDevices.getCount());
+  Serial.println("Scan done!");
+  pBLEScan->clearResults();   // delete results fromBLEScan buffer to release memory
+  delay(2000);
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////// esp32 server AHT20 //////////////////////////////////////////////////////////////
+
+/*********
+  knandas
+  esp32 BLE server + AHT20
+*********/
+#include <Arduino.h>
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+#include <Wire.h>
+#include <AHT20.h>
+AHT20 aht20;
+
+#define uS_TO_S_FACTOR 1000000ULL  // Conversion factor for micro seconds to seconds
+#define TIME_TO_SLEEP  2
+
+//Default Temperature is in Celsius
+//Comment the next line for Temperature in Fahrenheit
+#define temperatureCelsius
+
+//BLE server name
+#define bleServerName "AHT20_ESP32"
+
+
+float temp;
+float tempF;
+float hum;
+
+// Timer variables
+unsigned long lastTime = 0;
+unsigned long timerDelay = 30000;
+
+bool deviceConnected = false;
+bool disConDet = false;
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+#define SERVICE_UUID "91bad492-b950-4226-aa2b-4ede9fa42f59"
+
+// Temperature Characteristic and Descriptor
+#ifdef temperatureCelsius
+  BLECharacteristic ahtTemperatureCelsiusCharacteristics("cba1d466-344c-4be3-ab3f-189f80dd7518", BLECharacteristic::PROPERTY_NOTIFY);
+  BLEDescriptor ahtTemperatureCelsiusDescriptor(BLEUUID((uint16_t)0x2902));
+#else
+  BLECharacteristic ahtTemperatureFahrenheitCharacteristics("f78ebbff-c8b7-4107-93de-889a6a06d408", BLECharacteristic::PROPERTY_NOTIFY);
+  BLEDescriptor ahtTemperatureFahrenheitDescriptor(BLEUUID((uint16_t)0x2902));
+#endif
+
+// Humidity Characteristic and Descriptor
+BLECharacteristic ahtHumidityCharacteristics("ca73b3ba-39f6-4ab3-91ae-186dc9577d99", BLECharacteristic::PROPERTY_NOTIFY);
+BLEDescriptor ahtHumidityDescriptor(BLEUUID((uint16_t)0x2903));
+
+//Setup callbacks onConnect and onDisconnect
+class MyServerCallbacks: public BLEServerCallbacks {
+  void onConnect(BLEServer* pServer) {
+    deviceConnected = true;
+    Serial.println("Connected");
+  };
+  void onDisconnect(BLEServer* pServer) {
+    deviceConnected = false;
+    Serial.println("Disconnected");
+    esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+    Serial.println("Going to sleep now");
+    esp_deep_sleep_start();
+  };
+};
+
+void initAHT()
+{
+  if (!aht20.begin()) 
+  {
+    Serial.println("AHT20 not detected. Please check wiring. Freezing.");
+    while (1);
+  }
+  Serial.println("AHT20 acknowledged.");
+}
+
+void setup() 
+{
+  // Start serial communication 
+  Serial.begin(115200);
+
+  // Init AHT Sensor
+  initAHT();
+
+  // Create the BLE Device
+  BLEDevice::init(bleServerName);
+
+  // Create the BLE Server
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *ahtService = pServer->createService(SERVICE_UUID);
+
+  // Create BLE Characteristics and Create a BLE Descriptor
+  // Temperature
+  #ifdef temperatureCelsius
+    ahtService->addCharacteristic(&ahtTemperatureCelsiusCharacteristics);
+    ahtTemperatureCelsiusDescriptor.setValue("AHT20 temperature Celsius");
+    ahtTemperatureCelsiusCharacteristics.addDescriptor(&ahtTemperatureCelsiusDescriptor);
+  #else
+    ahtService->addCharacteristic(&bahtTemperatureFahrenheitCharacteristics);
+    ahtTemperatureFahrenheitDescriptor.setValue("AHT20 temperature Fahrenheit");
+    ahtTemperatureFahrenheitCharacteristics.addDescriptor(&ahtTemperatureFahrenheitDescriptor);
+  #endif  
+
+  // Humidity
+  ahtService->addCharacteristic(&ahtHumidityCharacteristics);
+  ahtHumidityDescriptor.setValue("AHT20 humidity");
+  ahtHumidityCharacteristics.addDescriptor(new BLE2902());
+  
+  // Start the service
+  ahtService->start();
+
+  // Start advertising
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pServer->getAdvertising()->start();
+  Serial.println("Waiting a client connection to notify...");
+
+}
+
+void loop() 
+{
+  if (deviceConnected) 
+  {
+    if ((millis() - lastTime) > timerDelay) 
+    {
+      // Read temperature as Celsius (the default)
+      temp = aht20.getTemperature();
+      //temp = random(1,20);
+      // Fahrenheit
+      tempF = 1.8*temp +32;
+      // Read humidity
+      hum = aht20.getHumidity();
+      //hum = random(1,100);
+  
+      //Notify temperature reading from AHT20 sensor
+      #ifdef temperatureCelsius
+        static char temperatureCTemp[6];
+        dtostrf(temp, 6, 2, temperatureCTemp);
+        //Set temperature Characteristic value and notify connected client
+        ahtTemperatureCelsiusCharacteristics.setValue(temperatureCTemp);
+        ahtTemperatureCelsiusCharacteristics.notify();
+        Serial.print("Temperature Celsius: ");
+        Serial.print(temp);
+        Serial.print(" ºC");
+      #else
+        static char temperatureFTemp[6];
+        dtostrf(tempF, 6, 2, temperatureFTemp);
+        //Set temperature Characteristic value and notify connected client
+        ahtTemperatureFahrenheitCharacteristics.setValue(temperatureFTemp);
+        ahtTemperatureFahrenheitCharacteristics.notify();
+        Serial.print("Temperature Fahrenheit: ");
+        Serial.print(tempF);
+        Serial.print(" ºF");
+      #endif
+      
+      //Notify humidity reading from AHT20
+      static char humidityTemp[6];
+      dtostrf(hum, 6, 2, humidityTemp);
+      //Set humidity Characteristic value and notify connected client
+      ahtHumidityCharacteristics.setValue(humidityTemp);
+      ahtHumidityCharacteristics.notify();   
+      Serial.print(" - Humidity: ");
+      Serial.print(hum);
+      Serial.println(" %");
+      
+      lastTime = millis();
+    }
+  }else
+  {
+    if(disConDet==0) 
+    {
+      lastTime=millis();
+      disConDet=1;
+    }  
+    
+    if(disConDet==1&&millis()-lastTime>5000)
+    {
+      esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+      Serial.println("Going to sleep now");
+      esp_deep_sleep_start();
+    }
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////// esp32 client AHT20 //////////////////////////////////////////////////////////////
+/*********
+ knandas
+ esp32_BLE_client AHT20
+*********/
+
+#include "BLEDevice.h"
+
+
+//Default Temperature is in Celsius
+//Comment the next line for Temperature in Fahrenheit
+#define temperatureCelsius
+
+//BLE Server name (the other ESP32 name running the server sketch)
+#define bleServerName "AHT20_ESP32"
+
+/* UUID's of the service, characteristic that we want to read*/
+// BLE Service
+static BLEUUID ahtServiceUUID("91bad492-b950-4226-aa2b-4ede9fa42f59");
+
+// BLE Characteristics
+#ifdef temperatureCelsius
+  //Temperature Celsius Characteristic
+  static BLEUUID temperatureCharacteristicUUID("cba1d466-344c-4be3-ab3f-189f80dd7518");
+#else
+  //Temperature Fahrenheit Characteristic
+  static BLEUUID temperatureCharacteristicUUID("f78ebbff-c8b7-4107-93de-889a6a06d408");
+#endif
+
+// Humidity Characteristic
+static BLEUUID humidityCharacteristicUUID("ca73b3ba-39f6-4ab3-91ae-186dc9577d99");
+
+//Flags stating if should begin connecting and if the connection is up
+static boolean doConnect = false;
+static boolean connected = false;
+
+//Address of the peripheral device. Address will be found during scanning...
+static BLEAddress *pServerAddress;
+ 
+//Characteristicd that we want to read
+static BLERemoteCharacteristic* temperatureCharacteristic;
+static BLERemoteCharacteristic* humidityCharacteristic;
+
+//Activate notify
+const uint8_t notificationOn[] = {0x1, 0x0};
+const uint8_t notificationOff[] = {0x0, 0x0};
+
+//Variables to store temperature and humidity
+char* temperatureChar;
+char* humidityChar;
+
+//Flags to check whether new temperature and humidity readings are available
+boolean newTemperature = false;
+boolean newHumidity = false;
+
+//Connect to the BLE Server that has the name, Service, and Characteristics
+bool connectToServer(BLEAddress pAddress) {
+   BLEClient* pClient = BLEDevice::createClient();
+ 
+  // Connect to the remove BLE Server.
+  pClient->connect(pAddress);
+  Serial.println(" - Connected to server");
+ 
+  // Obtain a reference to the service we are after in the remote BLE server.
+  BLERemoteService* pRemoteService = pClient->getService(ahtServiceUUID);
+  if (pRemoteService == nullptr) {
+    Serial.print("Failed to find our service UUID: ");
+    Serial.println(ahtServiceUUID.toString().c_str());
+    return (false);
+  }
+ 
+  // Obtain a reference to the characteristics in the service of the remote BLE server.
+  temperatureCharacteristic = pRemoteService->getCharacteristic(temperatureCharacteristicUUID);
+  humidityCharacteristic = pRemoteService->getCharacteristic(humidityCharacteristicUUID);
+
+  if (temperatureCharacteristic == nullptr || humidityCharacteristic == nullptr) {
+    Serial.print("Failed to find our characteristic UUID");
+    return false;
+  }
+  Serial.println(" - Found our characteristics");
+ 
+  //Assign callback functions for the Characteristics
+  temperatureCharacteristic->registerForNotify(temperatureNotifyCallback);
+  humidityCharacteristic->registerForNotify(humidityNotifyCallback);
+  return true;
+}
+
+//Callback function that gets called, when another device's advertisement has been received
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks 
+{
+  void onResult(BLEAdvertisedDevice advertisedDevice) 
+  {
+    if (advertisedDevice.getName() == bleServerName) 
+    { //Check if the name of the advertiser matches
+      advertisedDevice.getScan()->stop(); //Scan can be stopped, we found what we are looking for
+      pServerAddress = new BLEAddress(advertisedDevice.getAddress()); //Address of advertiser is the one we need
+      doConnect = true; //Set indicator, stating that we are ready to connect
+      Serial.println("Device found. Connecting!");
+    }
+  }
+};
+ 
+//When the BLE Server sends a new temperature reading with the notify property
+static void temperatureNotifyCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic, 
+                                        uint8_t* pData, size_t length, bool isNotify) {
+  //store temperature value
+  temperatureChar = (char*)pData;
+  newTemperature = true;
+}
+
+//When the BLE Server sends a new humidity reading with the notify property
+static void humidityNotifyCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic, 
+                                    uint8_t* pData, size_t length, bool isNotify) {
+  //store humidity value
+  humidityChar = (char*)pData;
+  newHumidity = true;
+}
+
+
+void printReadings()
+{
+  Serial.print("Temperature:");
+  Serial.print(temperatureChar);
+  #ifdef temperatureCelsius
+    //Temperature Celsius
+    Serial.print("C");
+  #else
+    //Temperature Fahrenheit
+    Serial.print("F");
+  #endif
+  Serial.print(" Humidity:");
+  Serial.print(humidityChar); 
+  Serial.println("%");
+}
+
+void setup() 
+{
+  //Start serial communication
+  Serial.begin(115200);
+  Serial.println("Starting Arduino BLE Client application...");
+
+  //Init BLE device
+  BLEDevice::init("");
+ 
+  // Retrieve a Scanner and set the callback we want to use to be informed when we
+  // have detected a new device.  Specify that we want active scanning and start the
+  // scan to run for 30 seconds.
+  BLEScan* pBLEScan = BLEDevice::getScan();
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setActiveScan(true);
+  pBLEScan->start(30);
+}
+
+void loop() 
+{
+  // If the flag "doConnect" is true then we have scanned for and found the desired
+  // BLE Server with which we wish to connect.  Now we connect to it.  Once we are
+  // connected we set the connected flag to be true.
+  if (doConnect == true) 
+  {
+    if (connectToServer(*pServerAddress)) 
+    {
+      Serial.println("We are now connected to the BLE Server.");
+      //Activate the Notify property of each Characteristic
+      temperatureCharacteristic->getDescriptor(BLEUUID((uint16_t)0x2902))->writeValue((uint8_t*)notificationOn, 2, true);
+      humidityCharacteristic->getDescriptor(BLEUUID((uint16_t)0x2902))->writeValue((uint8_t*)notificationOn, 2, true);
+      connected = true;
+    } else 
+    {
+      Serial.println("We have failed to connect to the server; Restart your device to scan for nearby BLE server again.");
+    }
+    doConnect = false;
+  }
+  //if new temperature readings are available, 
+  if (newTemperature && newHumidity){
+    newTemperature = false;
+    newHumidity = false;
+    printReadings();
+  }
+  delay(1000); // Delay a second between loops.
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
